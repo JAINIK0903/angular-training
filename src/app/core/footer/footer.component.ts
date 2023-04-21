08478cfc462171a54896a7ec8565e7a222e6ecd0
Jainik090302 <jainik.suthar@bacancy.com>
2023-04-21 14:39:17 +0530

    added a email with subscribe button in footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  //email validation
+  public email: string = '';
+  public patternForEmail: RegExp =
+    /^\w+([\.\-]?\w+)*@\w+([\.\-]?\w+)*(\.\w{2,})+$/;
 
+  public validateEmail(): void {
+    if (this.patternForEmail.test(this.email)) {
+      console.log(this.email + ' - ' + 'You are now subscriber.');
+    } else {
+      alert('Enter valid email address...!!');
+    }
+    this.email = '';
+  }
 }
