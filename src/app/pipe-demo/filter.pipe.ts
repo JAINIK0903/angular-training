44783a8847c379b2ba79c37e340802b050a2c7cd
Jainik090302 <jainik.suthar@bacancy.com>
2023-05-10 10:06:53 +0530

    add a pipe module, create a custom pipe and create a pipe component

--- src/app/pipe-demo/filter.pipe.ts
+++ src/app/pipe-demo/filter.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'mtoCm',
 })
 export class FilterPipe implements PipeTransform {
-  transform(value: number, ...args: number[]) {
+  transform(value: number) {
     return value * 100;
   }
 }
